b1e0b833356235a24213f03ebe4ed5c380c20f99
Ray McDermott <ray.mcdermott@toyota-europe.com>
2015-10-09 17:03:21 +0200

    rules as data

--- src/car_configurator/min_parser.clj
+++ src/car_configurator/min_parser.clj
@@ -8,29 +8,29 @@
 
 (defrecord ValidFuel [fuel])
 
-(defrecord Suffix [model fuel transmission colour])
+(defrecord PartiallyConfiguredCar [model fuel transmission colour])
 
 ; this is what could come from a DB
-(def somename-suffixes [{:model :somename :fuel :diesel :transmission :automatic :colour :black}
-                        {:model :somename :fuel :diesel :transmission :manual :colour :black}
-                        {:model :somename :fuel :petrol :transmission :manual :colour :black}
-                        {:model :somename :fuel :diesel :transmission :automatic :colour :red}
-                        {:model :somename :fuel :diesel :transmission :manual :colour :red}])
+(def partial-configurations [{:model :somename :fuel :diesel :transmission :automatic :colour :black}
+                             {:model :somename :fuel :diesel :transmission :manual :colour :black}
+                             {:model :somename :fuel :petrol :transmission :manual :colour :black}
+                             {:model :somename :fuel :diesel :transmission :automatic :colour :red}
+                             {:model :somename :fuel :diesel :transmission :manual :colour :red}])
 
 ; base data as 'good' data into the fact space
-(def suffixes (map #(->Suffix (:model %)
-                              (:fuel %)
-                              (:transmission %)
-                              (:colour %)) somename-suffixes))
+(def suffixes (map #(->PartiallyConfiguredCar (:model %)
+                                              (:fuel %)
+                                              (:transmission %)
+                                              (:colour %)) partial-configurations))
 
-(def valid-fuels (set (map #(:fuel %) somename-suffixes)))
-(def valid-colours (set (map #(:colour %) somename-suffixes)))
-(def valid-transmissions (set (map #(:transmission %) somename-suffixes)))
+(def valid-fuels (set (map #(:fuel %) partial-configurations)))
+(def valid-colours (set (map #(:colour %) partial-configurations)))
+(def valid-transmissions (set (map #(:transmission %) partial-configurations)))
 
 
 (defrule check-valid-fuel
          "Inserts an InvalidOption fact if the model presents an invalid fuel."
-         [?fuel-count <- (acc/distinct :fuel) :from [Suffix]]
+         [?fuel-count <- (acc/distinct :fuel) :from [PartiallyConfiguredCar]]
          [:test (not (empty? (set/difference ?fuel-count valid-fuels)))]
          =>
          (println "fails on" (set/difference ?fuel-count valid-fuels)))
@@ -40,59 +40,150 @@
                     (insert-all suffixes)
                     (insert-all valid-fuels)
                     ; and a bad one
-                    (insert (->Suffix :somename :jibberish :manual :red)))]
+                    (insert (->PartiallyConfiguredCar :somename :jibberish :manual :red)))]
     (comment time (fire-rules session))))
 
-(run-examples)
+;(run-examples)
 
 (defn valid-option-accum [key]
   (acc/distinct key))
 
-(defn partial-eq [collection]
+(defn valid-option-partial-eq [collection]
   (partial = collection))
 
 (defn partial-invalid-option [key]
   (partial ->InvalidOption key))
 
+(defn partial-diff [collection]
+  (let [func (fn [valid-range presented-range]
+               (set/difference presented-range valid-range))]
+    (partial func collection)))
+
+(defn partial-validity-check [collection]
+  (let [func (fn [valid-range presented-range] (not (= valid-range presented-range)))]
+    (partial func collection)))
+
+(def partial-colours-diff (partial-diff valid-colours))
+
+(def partial-transmission-diff (partial-diff valid-transmissions))
+
+(def partial-colour-validity (partial-validity-check valid-colours))
+
+(def partial-transmission-validity (partial-validity-check valid-transmissions))
+
 ; TODO.... now generate collections, rules bindings and RHS to create InvalidOption
 ; constraints and rules are all generated!!
 
+;(def valid-fuels (set (map #(:fuel %) somename-suffixes)))
+
 (def fuel-accum (valid-option-accum :fuel))
-(def partial-eq-fuels (partial-eq valid-fuels))
 
-(defn run-examples2 []
-  (let [rules '[; Rules structure must be quoted so Clara can evaluate it...
+(def partial-eq-fuels (valid-option-partial-eq valid-fuels))
 
-                {:doc "This is the same rule as check-valid-fuel above but defined as data"
-                 :lhs [{:result-binding :?presented-options
-                        :accumulator    fuel-accum
-                        :from           {:type        car_configurator.min_parser.Suffix
-                                         :constraints []}}
-                       {:constraints [(not (partial-eq-fuels ?presented-options))]}]
-                 :rhs (partial-invalid-option "fail")
-                 ; TODO - defquery
-                 }
+(def partial-eq-colour (valid-option-partial-eq valid-colours))
 
-                {:lhs  [{:result-binding :?presented-options
-                         :accumulator    (acc/distinct :colour)
-                         :from           {:type        car_configurator.min_parser.Suffix
-                                          :constraints []}}
-                        {:constraints [(not (empty? (set/difference ?presented-options valid-colours)))]}]
-                 :rhs  (println "Fails on y:" (set/difference ?presented-options valid-colours))}
 
-                {:lhs  [{:result-binding :?presented-options
-                         :accumulator    (acc/distinct :transmission)
-                         :from           {:type        car_configurator.min_parser.Suffix
-                                          :constraints []}}
-                        {:constraints [(not (empty? (set/difference ?presented-options valid-transmissions)))]}]
-                 :rhs  (println "Fails on z:" (set/difference ?presented-options valid-transmissions))}
-                ]
-        session (-> (mk-session rules)
+; and now use what we know to generate the rules...
+(defn derive-valid-option [training-data option]
+  (set (map #(option %) training-data)))
+
+
+;(def map-of-options "")
+;(def accum-rule-template {:lhs [{:result-binding (keyword "?result")
+;                                 :accumulator    (valid-option-accum "<<key>>")
+;                                 :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+;                                                  :constraints []}}
+;                                {:constraints [((partial-validity-check "<<collection>>") (symbol "?result"))]}]
+;                          :rhs (partial-invalid-option (str "Fails on key: " "<<key>>" ((partial-diff "<<collection>>") (symbol "?result"))))})
+
+
+(defn gen-rule [type training-data option-to-check]
+  (let [valid-options (derive-valid-option training-data option-to-check)
+        result-binding-keyword (keyword "?result")
+        accumulator (valid-option-accum option-to-check)
+        from {:type type :constraints []}
+        constraints (vector '(partial-validity-check valid-options ?result))
+        rhs '(println (str "Fails on value: " ((partial-diff valid-options) ?result)))
+        ]
+    {:lhs [{:result-binding result-binding-keyword
+            :accumulator    accumulator
+            :from           from}
+           {:constraints constraints}]
+     :rhs rhs}))
+
+(def options-to-check #{:fuel :transmission :colour})
+
+(def typeToValidate car_configurator.min_parser.PartiallyConfiguredCar)
+
+(def fuel-generated-rule
+  (gen-rule typeToValidate partial-configurations :fuel))
+
+(defn gen-rules [training-data options-to-check]
+  (map #(gen-rule typeToValidate training-data %) options-to-check))
+
+(def colour-rule-as-data
+  '{:lhs [{:result-binding :?presented-options
+           :accumulator    (acc/distinct :colour)
+           :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                            :constraints []}}
+          {:constraints [(partial-colour-validity ?presented-options)]}]
+    :rhs (println "Fails on colours all partial everywhere:" (partial-colours-diff ?presented-options))})
+
+(def transmission-rule-as-data
+  '{:lhs [{:result-binding :?presented-options
+           :accumulator    (acc/distinct :transmission)
+           :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                            :constraints []}}
+          {:constraints [(partial-transmission-validity ?presented-options)]}]
+    :rhs (println "Fails on simplified partial diffz:" (partial-transmission-diff ?presented-options))})
+
+(def rules (vector colour-rule-as-data transmission-rule-as-data))
+
+(defn run-rules-as-data []
+  (let [session (-> (mk-session rules)
                     (insert-all suffixes)
                     ; and some bad ones
-                    (insert (->Suffix :somename :diesel :bizarro :red))
-                    (insert (->Suffix :somename :hybrid :automatic :red))
-                    (insert (->Suffix :somename :diesel :manual :yellow)))]
-    (time (fire-rules session))))
+                    (insert (->PartiallyConfiguredCar :somename :diesel :bizarro :red))
+                    (insert (->PartiallyConfiguredCar :somename :hybrid :automatic :red))
+                    (insert (->PartiallyConfiguredCar :somename :diesel :manual :yellow))
+                    )]
+    (fire-rules session)))
 
-(run-examples2)
+(run-rules-as-data)
+
+(comment defn run-examples2 []
+         (let
+           [rules '[; Rules structure must be quoted so Clara can evaluate it...
+
+                    fuel-generated-rule
+
+                    ;{:lhs [{:result-binding :?presented-options
+                    ;        :accumulator    fuel-accum
+                    ;        :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                    ;                         :constraints []}}
+                    ;       {:constraints [(not (partial-eq-fuels ?presented-options))]}]
+                    ; :rhs (partial-invalid-option "fail fast")}
+
+                    {:lhs [{:result-binding :?presented-options
+                            :accumulator    (acc/distinct :colour)
+                            :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                                             :constraints []}}
+                           {:constraints [(partial-colour-validity ?presented-options)]}]
+                     :rhs (println "Fails on colours all partial everywhere:" (partial-colours-diff ?presented-options))}
+
+                    {:lhs [{:result-binding :?presented-options
+                            :accumulator    (acc/distinct :transmission)
+                            :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                                             :constraints []}}
+                           {:constraints [(partial-transmission-validity ?presented-options)]}]
+                     :rhs (println "Fails on simplified partial diffz:" (partial-transmission-diff ?presented-options))}
+                    ]
+            session (-> (mk-session rules)
+                        (insert-all suffixes)
+                        ; and some bad ones
+                        (insert (->PartiallyConfiguredCar :somename :diesel :bizarro :red))
+                        (insert (->PartiallyConfiguredCar :somename :hybrid :automatic :red))
+                        (insert (->PartiallyConfiguredCar :somename :diesel :manual :yellow)))]
+           (time (fire-rules session))))
+
+;(run-examples2)
---
+++ src/car_configurator/partial_configuration.clj
@@ -0,0 +1,118 @@
+(ns car-configurator.partial-configuration
+  (:refer-clojure :exclude [==])
+  (:require [clojure.set :as set]
+            [clara.rules.accumulators :as acc]
+            [clara.rules :refer :all]))
+
+; get all the models / ssn / kata combinations
+
+; get the suffixes for each model
+
+; get the equipment for each suffix (configurable bill of materials)
+
+; ok so now we have the configurable bill of materials for each suffix ...
+
+; can we use Clara to detect the truth about the truth for each piece of
+; equipment across the suffixes for this model?
+
+; Clara accumulators are for testing collections
+
+(defrecord InvalidOption [field message])
+
+(defrecord ValidFuel [fuel])
+
+(defrecord PartiallyConfiguredCar [model fuel transmission colour])
+
+; this is what could come from a DB
+(def partial-configurations [{:model :somename :fuel :diesel :transmission :automatic :colour :black}
+                             {:model :somename :fuel :diesel :transmission :manual :colour :black}
+                             {:model :somename :fuel :petrol :transmission :manual :colour :black}
+                             {:model :somename :fuel :diesel :transmission :automatic :colour :red}
+                             {:model :somename :fuel :diesel :transmission :manual :colour :red}])
+
+; base data as 'good' data into the fact space
+(def suffixes (map #(->PartiallyConfiguredCar (:model %)
+                                              (:fuel %)
+                                              (:transmission %)
+                                              (:colour %)) partial-configurations))
+
+; base functions
+(defn partial-diff [collection]
+  (let [func (fn [valid-range presented-range]
+               (set/difference presented-range valid-range))]
+    (partial func collection)))
+
+(defn partial-validity-check [collection]
+  (let [func (fn [valid-range presented-range]
+               (not (= valid-range presented-range)))]
+    (partial func collection)))
+
+; data maps
+;(def valid-fuels (set (map #(:fuel %) partial-configurations)))
+;(def valid-colours (set (map #(:colour %) partial-configurations)))
+;(def valid-transmissions (set (map #(:transmission %) partial-configurations)))
+;
+;; partial functions that combine the base functions and data maps
+;(def partial-fuel-diff (partial-diff valid-fuels))
+;(def partial-colours-diff (partial-diff valid-colours))
+;(def partial-transmission-diff (partial-diff valid-transmissions))
+;
+;(def partial-fuel-validity (partial-validity-check valid-fuels))
+;(def partial-colour-validity (partial-validity-check valid-colours))
+;(def partial-transmission-validity (partial-validity-check valid-transmissions))
+
+; so now at least come up with a map to produce these partials programmatically
+(defn partials-map [key]
+  (let [coll (set (map #(key %) partial-configurations))
+        differ (partial-diff coll)
+        validator (partial-validity-check coll)]
+    {:key       key
+     :differ    differ
+     :validator validator}))
+
+(defn generate-partials [keys]
+  (set (map #(partials-map %) keys)))
+
+(def keys #{:model :fuel :transmission})
+
+(def partials (generate-partials keys))
+
+; the partial functions are used within the data to implement the constraints
+
+(def fuel-rule-as-data
+  '{:lhs [{:result-binding :?presented-options
+           :accumulator    (acc/distinct :fuel)
+           :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                            :constraints []}}
+          {:constraints [(partial-fuel-validity ?presented-options)]}]
+    :rhs (println "Fails on fuels all partial everywhere:" (partial-fuel-diff ?presented-options))})
+
+(def colour-rule-as-data
+  '{:lhs [{:result-binding :?presented-options
+           :accumulator    (acc/distinct :colour)
+           :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                            :constraints []}}
+          {:constraints [(partial-colour-validity ?presented-options)]}]
+    :rhs (println "Fails on colours all partial everywhere:" (partial-colours-diff ?presented-options))})
+
+(def transmission-rule-as-data
+  '{:lhs [{:result-binding :?presented-options
+           :accumulator    (acc/distinct :transmission)
+           :from           {:type        car_configurator.min_parser.PartiallyConfiguredCar
+                            :constraints []}}
+          {:constraints [(partial-transmission-validity ?presented-options)]}]
+    :rhs (println "Fails on simplified partial diffz:" (partial-transmission-diff ?presented-options))})
+
+(def rules (vector fuel-rule-as-data colour-rule-as-data transmission-rule-as-data))
+
+(defn run-rules-as-data []
+  (let [session (-> (mk-session rules)
+                    (insert-all suffixes)
+                    ; and some bad ones
+                    (insert (->PartiallyConfiguredCar :somename :diesel :bizarro :red))
+                    (insert (->PartiallyConfiguredCar :somename :hybrid :automatic :red))
+                    (insert (->PartiallyConfiguredCar :somename :diesel :manual :yellow))
+                    )]
+    (fire-rules session)))
+
+(run-rules-as-data)
